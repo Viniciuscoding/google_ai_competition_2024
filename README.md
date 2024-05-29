2884a343339a6ed08aa94c4df4b8fb00a82d5697
Viniciuscoding <vin.idem@gmail.com>
2024-05-29 10:03:04 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,77 @@
+# Gemini Hackathon 2024
+Deloitte Team Submission GCP/Gemini Hackathon. Showcasing a Youtube Multimodal analysis chrome plugin.
+
+## Youtube Multimodal Video Analysis Chrome Plugin
+## Team Members:
+### Vinicus Granja
+### Jay Park
+### Pradeep Mohan
+### Antoine
+### Hector
+### Andrew Irwin
+### Trenten Babcock
+
+
+# YouTube-Transcript-Summarizer
+
+**Youtube Transcript Summarizer** is a Chrome Extension that allows users to get a **summarized** version of the transcripts of YouTube videos with a **single click**. It is built on a **Flask Backend REST API** to expose the summarization service to the client.
+
+
+## Project Stages
+![alt text](/extension/images/stages.png?raw=true)
+
+
+## Features
+- Allows the user to adjust the maximum length of the summarized text through dynamic truncation.
+- Adopts a language-agnostic approach and supports transcript summarization even for videos without subtitles.
+- Employs an asynchronous XMLHttpRequest to ensure non-blocking communication with the Flask Backend.
+
+
+## Output Screenshot
+![alt text](/extension/images/screenshot-youtube-summary.png?raw=true)
+
+
+## Youtube Link to Demo
+[![Watch the video](https://img.youtube.com/vi/bu5BQ0PadBo/default.jpg)](https://www.youtube.com/watch?v=bu5BQ0PadBo)
+
+## Installation
+- Clone this repository to your local machine:
+-
+  ```
+  git clone https://github.com/<your-username>/geminihackathon24.git
+
+  cd YouTube-Transcript-Summarizer
+  ```
+
+- Create the conda environment using the `environment.yml` file: `conda env create -f environment.yml`
+- Activate the conda environment: `conda activate hackathonEnv`
+- Next, install the dependencies:
+-
+  ```
+  pip install -r Requirements.txt
+  ```
+
+- Next create a .env file with your gemini API key `GOOGLE_API_KEY=<your-key-here>`
+- To execute the Application locally:
+  - Start the Flask backend on the terminal using the following command:
+
+    ```
+    python TranscriptApp.py
+    ```
+    This will start a local server at ```http://127.0.0.1:5000/```. You may see a couple of warnings but it's all good and you may ignore it!
+  - Load the extension into Google Chrome:
+    - Open Google Chrome and go to ```chrome://extensions/```.
+    - Enable the ```Developer mode``` toggle in the top right corner.
+    - Click on ```Load unpacked``` and then select the folder ```extension``` inside the directory folder ```geminihackathon24```. The one you cloned from this repository.
+  - You should now see the extension loaded in the Chrome toolbar. Navigate to any YouTube video, click on the extension icon, and click "Summarize" to see the summary of the video   transcript.
+  - All Done..!!
+
+
+## Contribution
+Contributions to this project are *welcome!* If you wish to contribute, please follow these steps:
+- Fork the repository.
+- Create a new branch for your features or fixes.
+- Make your changes and commit them.
+- Push your changes to your fork.
+- Create a Pull Request from your fork to this repository.
+- Make sure to update the ```Requirements.txt``` file if you've added any new dependencies.
---
+++ TranscriptApp.py
@@ -0,0 +1,115 @@
+# Import all the necessary dependencies
+import os
+from flask import Flask, request, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi
+from langdetect import detect
+import google.generativeai as genai
+from dotenv import load_dotenv
+from Gemini_Video_Summary import Gemini_Summarization
+from configured_chat import ConfiguredChat
+from notebooks.topic_detection_function import detect_topics_sentiment
+
+load_dotenv()  # Load environment variables from .env file
+application = Flask(__name__)
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+chat_instance = ConfiguredChat(
+    project_id="vertexai-gemini-hackathon-2024",
+    model_name="gemini-1.5-pro-preview-0409",
+)
+
+chat_instance = chat_instance.model.start_chat()
+
+
+@application.get("/summary")
+def summary_api():
+    """
+    Summarizes the transcript of a YouTube video.
+
+    This function takes a YouTube video URL and an optional max_length parameter as inputs.
+    It first retrieves the transcript of the YouTube video.
+    If the transcript is longer than 3000 words, it uses extractive summarization (e.g. LSA).
+    Otherwise, it uses abstractive summarization.
+
+    Parameters:
+    - url (str): The URL of the YouTube video.
+    - max_length (int, optional): The maximum length of the summary. Defaults to 150.
+
+    Returns:
+    - str: The summarized transcript.
+    - int: HTTP status code (200 for success, 404 for failure).
+    """
+    url = request.args.get("url", "")
+    max_length = int(request.args.get("max_length", 150))
+    video_id = url.split("=")[1]
+    summarizer = Gemini_Summarization()
+
+    try:
+        transcript = get_transcript(video_id)
+        # print(f"transcript: {transcript}\n")
+    except:
+        return "No subtitles available for this video", 404
+
+    try:
+        # final_summary, combined_summaries = summarizer.complete_summarization(
+        #     transcript, is_YU_url=False
+        # )
+        sentiment_topic = detect_topics_sentiment(transcript)
+        print(f"sentiment: {sentiment_topic}\n")
+        # print(f"summary: {final_summary}\n")
+    except Exception as e:
+        print(f"Error occurred during summarization: {str(e)}")
+        return "An error occurred during summarization. Please try again later.", 500
+
+    return sentiment_topic, 200
+    # return final_summary, sentiment_topic, 200
+
+def is_transcript_english(transcript):
+    """
+    Detect if the transcript is primarily in English.
+
+    :param transcript: The transcript text to be analyzed.
+    :return: True if the transcript is primarily in English, False otherwise.
+    """
+    try:
+        language = detect(transcript)
+        return language == "en"
+
+    except Exception as e:
+        return False
+
+
+def get_transcript(video_id):
+    """
+    Fetches and concatenates the transcript of a YouTube video.
+
+    Parameters:
+    video_id (str): The ID of the YouTube video.
+
+    Returns:
+    str: A string containing the concatenated transcript of the video.
+
+    Raises:
+    Exception: If there is an error in fetching the transcript.
+    """
+    try:
+        transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+    except Exception as e:
+        raise e
+
+    transcript = " ".join([d["text"] for d in transcript_list])
+    return transcript
+
+@application.route("/chat", methods=["POST"])
+def chat():
+    data = request.get_json()
+    user_message = data["message"]
+
+    response = chat_instance.send_message(user_message)
+    response_text = response.text
+    print(response_text)
+    return response_text
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
